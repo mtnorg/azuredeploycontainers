7cce5ef69631bd8a063b17341999106533ccc6b6
Sapna <151709872+mtnorg@users.noreply.github.com>
2024-03-18 15:33:22 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header className='App-header'>
       <p>
-        Edit <code>src/App.tsx</code> and save to reload. hello world first time!
+        Edit <code>src/App.tsx</code> and save to reload. hello world container!
       </p>
       <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
         Learn React
